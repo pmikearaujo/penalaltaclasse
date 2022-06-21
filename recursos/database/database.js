32cc0e0e752980a6fd07567d902351269ea756c4
ojuarasobral <mikearaujoclassealta@gmail.com>
2022-06-21 15:15:25 -0300

    first commit

---
+++ recursos/database/database.js
@@ -0,0 +1,465 @@
+var array_codigo_penal = [{
+    "id_pena": "1",
+    "id_tabela_pm": "1",
+    "descricao": "Alta Velocidade",
+    "observacao": "Transitar com velocidade superior a permitida na via\t",
+    "multa": "5000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "2",
+    "id_tabela_pm": "2",
+    "descricao": "Estacionar em local proibido",
+    "observacao": "Estacionar em local proibido",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "3",
+    "id_tabela_pm": "3",
+    "descricao": "Abandono de Ve\u00edculo",
+    "observacao": "Abandonar ve\u00edculos em vias p\u00fablicas",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "4",
+    "id_tabela_pm": "4",
+    "descricao": "Parar em fila dupla",
+    "observacao": "Estacionar em fila dupla",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "5",
+    "id_tabela_pm": "5",
+    "descricao": "Transitar com som alto no autom\u00f3vel",
+    "observacao": "Transitar com som alto em qualquer ve\u00edculo",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "6",
+    "id_tabela_pm": "6",
+    "descricao": "Bloquear via sem autoriza\u00e7\u00e3o",
+    "observacao": "Bloquear vias p\u00fablicas com ve\u00edculos ou barreiras sem alvar\u00e1 e sem permiss\u00e3o",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "7",
+    "id_tabela_pm": "7",
+    "descricao": "Manobras perigosas",
+    "observacao": "Realizar manobras arriscadas em local n\u00e3o determinado",
+    "multa": "20000",
+    "pena": "20",
+    "fianca": "20000"
+}, {
+    "id_pena": "8",
+    "id_tabela_pm": "8",
+    "descricao": "Dire\u00e7\u00e3o Perigosa",
+    "observacao": "Transitar na contra m\u00e3o, utilizando a moto com apenas uma roda no ch\u00e3o, etc",
+    "multa": "20000",
+    "pena": "20",
+    "fianca": "20000"
+}, {
+    "id_pena": "9",
+    "id_tabela_pm": "9",
+    "descricao": "Dirigir sobre o efeito de alucin\u00f3genos",
+    "observacao": "Dirigir ap\u00f3s fazer o uso de bebidas e entorpecentes",
+    "multa": "20000",
+    "pena": "20",
+    "fianca": "20000"
+}, {
+    "id_pena": "10",
+    "id_tabela_pm": "10",
+    "descricao": "Portar traje m\u00e1scara",
+    "observacao": "Uso de m\u00e1scaras n\u00e3o autorizado",
+    "multa": "10000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "11",
+    "id_tabela_pm": "11",
+    "descricao": "Drogas para uso pr\u00f3prio",
+    "observacao": "Drogas at\u00e9 5(cinco) unidades de entorpecentes para uso pr\u00f3prio",
+    "multa": "10000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "12",
+    "id_tabela_pm": "12",
+    "descricao": "Dinheiro sujo abaixo de $50.000",
+    "observacao": "Ter posse de dinheiro sujo abaixo da quantia de $50.000",
+    "multa": "20000",
+    "pena": "0",
+    "fianca": null
+}, {
+    "id_pena": "13",
+    "id_tabela_pm": "13",
+    "descricao": "Porte de algemas\/capuz\/keycard",
+    "observacao": "Ter posse de algemas, capuzes ou keycards sem autoriza\u00e7\u00e3o",
+    "multa": "20000",
+    "pena": "20",
+    "fianca": "20000"
+}, {
+    "id_pena": "14",
+    "id_tabela_pm": "14",
+    "descricao": "Porte de masterpick\/lockpick",
+    "observacao": "Ter posse de masterpick ou lockpick",
+    "multa": "20000",
+    "pena": "20",
+    "fianca": "20000"
+}, {
+    "id_pena": "15",
+    "id_tabela_pm": "15",
+    "descricao": "Portar\/possuir colete bal\u00edstico acima de duas(2) unidades",
+    "observacao": "Possuir\/portar dois(2) ou mais coletes bal\u00edsticos",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "16",
+    "id_tabela_pm": "16",
+    "descricao": "Ve\u00edculo Clonado",
+    "observacao": "Ter posse de um ve\u00edculo clonado",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "17",
+    "id_tabela_pm": "17",
+    "descricao": "Dinheiro sujo igual ou acima de $50.000",
+    "observacao": "Ter posse de dinheiro sujo igual ou acima de $50.000",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "18",
+    "id_tabela_pm": "18",
+    "descricao": "Itens de fabrica\u00e7\u00e3o armas\/drogas\/muni\u00e7\u00f5es\/dinheiro",
+    "observacao": "Possuir quaisquer quantidades de itens de fabrica\u00e7\u00e3o",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "19",
+    "id_tabela_pm": "19",
+    "descricao": "Tr\u00e1fico de drogas",
+    "observacao": "Ter posse de 6(seis) ou mais unidades de entorpecentes",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "20",
+    "id_tabela_pm": "20",
+    "descricao": "Portar\/possuir muni\u00e7\u00e3o ilegal",
+    "observacao": "Portar\/possuir qualquer quantidade de muni\u00e7\u00f5es ilegais sem autoriza\u00e7\u00e3o. Sendo elas: FiveSeven, MTAR-21, AK-47, G36, Tec-9, Frajuta",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "21",
+    "id_tabela_pm": "21",
+    "descricao": "Omiss\u00e3o de socorro",
+    "observacao": "Deixar de acionar o socorro quando visualizar algu\u00e9m ferido",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "22",
+    "id_tabela_pm": "22",
+    "descricao": "Invas\u00e3o de propriedade",
+    "observacao": "Entrar em propriedade privada sem autoriza\u00e7\u00e3o",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "23",
+    "id_tabela_pm": "23",
+    "descricao": "Polui\u00e7\u00e3o sonora",
+    "observacao": "Ligar aparelhos sonoros em local p\u00fablico em volume extremamente elevado",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "24",
+    "id_tabela_pm": "24",
+    "descricao": "Resist\u00eancia a pris\u00e3o",
+    "observacao": "Resistir a deten\u00e7\u00e3o por qualquer meio",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "25",
+    "id_tabela_pm": "25",
+    "descricao": "Difama\u00e7\u00e3o",
+    "observacao": "Mentir\/ofender algu\u00e9m com a inten\u00e7\u00e3o de denegrir a sua imagem",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "26",
+    "id_tabela_pm": "26",
+    "descricao": "Tentativa de fuga",
+    "observacao": "Fugir da unidade policial ap\u00f3s dado a ordem de parada",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "27",
+    "id_tabela_pm": "27",
+    "descricao": "Perj\u00fario",
+    "observacao": "Prestar falso testemunho perante julgamento",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "28",
+    "id_tabela_pm": "28",
+    "descricao": "Tentativa de roubo ou furto",
+    "observacao": "Realizar tentativas de roubos ou furtos por qualquer meio",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "29",
+    "id_tabela_pm": "29",
+    "descricao": "Obstru\u00e7\u00e3o da justi\u00e7a",
+    "observacao": "Realizar ato para atrapalhar a justi\u00e7a de fazer sua fun\u00e7\u00e3o",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "30",
+    "id_tabela_pm": "30",
+    "descricao": "Falsidade Ideol\u00f3gica",
+    "observacao": "Alegar sem algu\u00e9m que n\u00e3o \u00e9",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "31",
+    "id_tabela_pm": "31",
+    "descricao": "Atentado ao pudor",
+    "observacao": "Nudez em locais p\u00fablicos",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "32",
+    "id_tabela_pm": "32",
+    "descricao": "Vandalismo",
+    "observacao": "Destruir ou danificar uma propriedade alheia de forma intencional",
+    "multa": "30000",
+    "pena": "30",
+    "fianca": "30000"
+}, {
+    "id_pena": "33",
+    "id_tabela_pm": "33",
+    "descricao": "Amea\u00e7a",
+    "observacao": "Utilizar de amea\u00e7a f\u00edsica ou moral contra algu\u00e9m",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "34",
+    "id_tabela_pm": "34",
+    "descricao": "Cal\u00fania",
+    "observacao": "Mentir sobre algu\u00e9m. Atribuir falsamente a algu\u00e9m um fato definido como crime",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "35",
+    "id_tabela_pm": "35",
+    "descricao": "Extors\u00e3o",
+    "observacao": "Agir com amea\u00e7a ou chantagem para obter lucros",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "36",
+    "id_tabela_pm": "36",
+    "descricao": "Suborno",
+    "observacao": "Ato ou efeito de subornar\/compra\/corrup\u00e7\u00e3o",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "37",
+    "id_tabela_pm": "37",
+    "descricao": "Les\u00e3o corporal",
+    "observacao": "Atentado a integridade f\u00edsica",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "38",
+    "id_tabela_pm": "38",
+    "descricao": "Corrida Ilegal",
+    "observacao": "Praticar rachas\/corridas n\u00e3o autorizadas com ve\u00edculos",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "39",
+    "id_tabela_pm": "39",
+    "descricao": "Porte de arma de baixo calibre",
+    "observacao": "Portar armas de baixo calibre (Pistolas e Submetralhadoras) sem autoriza\u00e7\u00e3o",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "40",
+    "id_tabela_pm": "40",
+    "descricao": "Furto",
+    "observacao": "Se apropriar de algo sem que o dono saiba ou autorize",
+    "multa": "40000",
+    "pena": "40",
+    "fianca": "40000"
+}, {
+    "id_pena": "41",
+    "id_tabela_pm": "41",
+    "descricao": "Roubo",
+    "observacao": "Uso de amea\u00e7a, agressividade ou tomar a for\u00e7a algo para si",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "42",
+    "id_tabela_pm": "42",
+    "descricao": "Forma\u00e7\u00e3o de quadrilha",
+    "observacao": "Estar em grupo maior ou igual a 3(tr\u00eas) para cometer crimes",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "43",
+    "id_tabela_pm": "43",
+    "descricao": "Assalto a loja de conveni\u00eancia",
+    "observacao": "Roubar lojas de conveni\u00eancia",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "44",
+    "id_tabela_pm": "44",
+    "descricao": "Assalto a caixa eletr\u00f4nico",
+    "observacao": "Roubar caixas eletr\u00f4nicos",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "45",
+    "id_tabela_pm": "45",
+    "descricao": "Plantar provas",
+    "observacao": "plantar provas para que algu\u00e9m seja acusado de um crime que n\u00e3o cometeu",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "46",
+    "id_tabela_pm": "46",
+    "descricao": "Provas Il\u00edcitas",
+    "observacao": "Provas obtidas violando alguns princ\u00edpios constitucionais ou direitos materiais",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "47",
+    "id_tabela_pm": "47",
+    "descricao": "Homic\u00eddio culposo",
+    "observacao": "Por neglig\u00eancia. Quando n\u00e3o h\u00e1 inten\u00e7\u00e3o de matar",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "48",
+    "id_tabela_pm": "48",
+    "descricao": "Tentativa de homic\u00eddio",
+    "observacao": "Tentar assassinar algu\u00e9m com inten\u00e7\u00e3o",
+    "multa": "50000",
+    "pena": "50",
+    "fianca": "50000"
+}, {
+    "id_pena": "49",
+    "id_tabela_pm": "49",
+    "descricao": "Sequestro",
+    "observacao": "Privar algu\u00e9m de sua liberdade",
+    "multa": "80000",
+    "pena": "80",
+    "fianca": null
+}, {
+    "id_pena": "50",
+    "id_tabela_pm": "50",
+    "descricao": "Portar armas de alto calibre",
+    "observacao": "Portar armas de alto calibre sem autoriza\u00e7\u00e3o",
+    "multa": "80000",
+    "pena": "80",
+    "fianca": null
+}, {
+    "id_pena": "51",
+    "id_tabela_pm": "51",
+    "descricao": "Tr\u00e1fico de armas",
+    "observacao": "Transitar\/possuir duas (2) ou mais armas de fogo sem autoriza\u00e7\u00e3o",
+    "multa": "80000",
+    "pena": "80",
+    "fianca": null
+}, {
+    "id_pena": "52",
+    "id_tabela_pm": "52",
+    "descricao": "Tr\u00e1fico de muni\u00e7\u00f5es",
+    "observacao": "Possuir\/Portar muni\u00e7\u00f5es acima de 100 unidades sem autoriza\u00e7\u00e3o, de qualquer calibre",
+    "multa": "80000",
+    "pena": "80",
+    "fianca": null
+}, {
+    "id_pena": "53",
+    "id_tabela_pm": "53",
+    "descricao": "Desacato ao servidor p\u00fablico",
+    "observacao": "Ofender um funcion\u00e1rio p\u00fablico no exerc\u00edcio de seu servi\u00e7o",
+    "multa": "80000",
+    "pena": "80",
+    "fianca": null
+}, {
+    "id_pena": "54",
+    "id_tabela_pm": "54",
+    "descricao": "Abuso de poder",
+    "observacao": "Abuso de poder por parte de um funcion\u00e1rio p\u00fablico",
+    "multa": "120000",
+    "pena": "120",
+    "fianca": null
+}, {
+    "id_pena": "55",
+    "id_tabela_pm": "55",
+    "descricao": "Assalto a Banco\/Joalheria",
+    "observacao": "Roubos\/Assaltos a bancos e joalherias",
+    "multa": "120000",
+    "pena": "120",
+    "fianca": null
+}, {
+    "id_pena": "56",
+    "id_tabela_pm": "56",
+    "descricao": "Corrup\u00e7\u00e3o passiva",
+    "observacao": "Corrup\u00e7\u00e3o cometida por um funcion\u00e1rio p\u00fablico",
+    "multa": "120000",
+    "pena": "120",
+    "fianca": null
+}, {
+    "id_pena": "57",
+    "id_tabela_pm": "57",
+    "descricao": "Latroc\u00ednio",
+    "observacao": "Roubo seguido de morte",
+    "multa": "120000",
+    "pena": "120",
+    "fianca": null
+}, {
+    "id_pena": "58",
+    "id_tabela_pm": "58",
+    "descricao": "Homic\u00eddio doloso",
+    "observacao": "Assassinar algu\u00e9m com plena consci\u00eancia e inten\u00e7\u00e3o",
+    "multa": "120000",
+    "pena": "120",
+    "fianca": null
+}]
